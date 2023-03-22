01417dc0d625dbdd9dde99df1a7dde481d02801f
SkareKrowMedia <64370361+SkareKrowMedia@users.noreply.github.com>
2023-03-22 09:27:23 -0500

    Add project files.

---
+++ UsbMonitor/Program.cs
@@ -0,0 +1,94 @@
+﻿using ConsoleApplication1;
+using System.Management;
+using USB_TEST;
+
+var Send = new SendMail();
+int ConnectedCount;
+int MailSentCount = 0;
+
+
+if (Environment.GetCommandLineArgs().Length > 1)
+{
+    string Argument = args[0];
+    switch (Argument)
+    {
+        case "/list":
+            var usbDevices = GetUSBDevices();
+            foreach (var usbDevice in usbDevices)
+            {
+                Console.WriteLine("Device ID: {0}, PNP Device ID: {1}, Description: {2}",
+                    usbDevice.DeviceID, usbDevice.PnpDeviceID, usbDevice.Description);
+            }
+            Environment.Exit(0);
+            break;
+        case "/device":
+            while (true)
+            {
+                ConnectedCount = 0;
+                foreach (var usbDevice in (List<USBDeviceInfo>?)GetUSBDevices())
+                {
+                if (usbDevice.DeviceID == args[1])
+                {
+                        if (ConnectedCount == 0) // is our device connected if so count it, maybe in the future we can do more than one device
+                        {
+                            ConnectedCount++;
+                        }
+                    }
+                }
+                Thread.Sleep(60000);
+                if (ConnectedCount == 0)
+                {
+                    if (MailSentCount == 0) // we only want to send mail once per run.
+                    {
+                        Send.Send(args[1]);
+
+                        MailSentCount++;
+                    }
+                }
+
+            }
+            break;
+    }
+
+    List<USBDeviceInfo> GetUSBDevices()
+    {
+        List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+        ManagementObjectCollection collection;
+        using (var searcher = new ManagementObjectSearcher(@"Select \* From Win32_USBHub"))
+            collection = searcher.Get();
+        foreach (var device in collection)
+        {
+            devices.Add(new USBDeviceInfo(
+            (string)device.GetPropertyValue("DeviceID"),
+            (string)device.GetPropertyValue("PNPDeviceID"),
+            (string)device.GetPropertyValue("Description")
+            ));
+        }
+        collection.Dispose();
+        return devices;
+    }
+}
+else
+{
+    Console.WriteLine("Please provide an argument.");
+    Console.WriteLine("Argument list below");
+    Console.WriteLine("list   shows all connected usb devices");
+    Console.WriteLine("Look for the device you want to monitor in the list and copy the device ID make sure to put in quotes.");
+    Console.WriteLine("device DEVICE STRING this should be the device id for example \"USB\\VID_18A5 & PID_0243\\07190BCB888D0545\"");
+    }
+namespace ConsoleApplication1
+{
+    class USBDeviceInfo
+    {
+        public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+        {
+            this.DeviceID = deviceID;
+            this.PnpDeviceID = pnpDeviceID;
+            this.Description = description;
+        }
+        public string DeviceID { get; private set; }
+        public string PnpDeviceID { get; private set; }
+        public string Description { get; private set; }
+    }
+}
+
---
+++ UsbMonitor/SendMail.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Net.Mail;
+
+using System.Net;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+using MailKit.Net.Smtp;
+
+using MimeKit;
+
+using static System.Net.Mime.MediaTypeNames;
+
+using MailKit.Security;
+
+namespace USB_TEST
+
+{
+
+    public class SendMail
+
+    {
+
+        public void Send(string v)
+
+        {
+
+            string deviceID = v;
+
+            string from_name = "USB Monitor"; // what the display name will be in the from field
+
+            string from_email = ""; //the email address you want the notification sent from.
+
+            string to_name = ""; //name of the person that you want the info being sent to
+
+            string to_email = ""; //the email address you want the notifcation sent to.
+
+            string subject = "Device Removed"; // the subject for the notification
+
+            int port = 587; //the smtp server port
+
+            string email_server = ""; //the email server port
+
+
+
+            var mailMessage = new MimeMessage();
+
+            mailMessage.From.Add(new MailboxAddress(from_name, from_email));
+
+            mailMessage.To.Add(new MailboxAddress(to_name, to_email));
+
+            mailMessage.Subject = subject;
+
+            mailMessage.Body = new TextPart("plain")
+
+            {
+
+                Text = "The USB device" + deviceID + " was removed at " + DateTime.Now.ToString() + " have a good day."
+
+            };
+
+            using (var smtpClient = new MailKit.Net.Smtp.SmtpClient())
+
+            {
+
+                smtpClient.Connect(email_server, port, SecureSocketOptions.StartTls);
+
+                smtpClient.Authenticate(from_email, "");
+
+                smtpClient.Send(mailMessage);
+
+                smtpClient.Disconnect(true);
+
+            }
+
+
+
+
+
+        }
+
+    }
+
+
+}
+
